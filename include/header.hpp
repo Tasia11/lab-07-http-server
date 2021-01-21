f6351a8e420ab589e28040d6aab83422a2be6db6
Tasia11 <61164926+Tasia11@users.noreply.github.com>
2021-01-21 08:12:56 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -1,34 +1,22 @@
 // Copyright 2020 Your Name <your_email>
 
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/bind.hpp>
-#include <ctime>
-#include <iostream>
-#include <vector>
-#include <utility>
-#include <thread>
-#include <boost/thread.hpp>
+#ifndef INCLUDE_HEADER_HPP_
+#define INCLUDE_HEADER_HPP_
 #include <nlohmann/json.hpp>
-#include <sstream>
 #include <string>
-#include <algorithm>
-#include <fstream>
-#include <memory>
-#include <mutex>
-#include <shared_mutex>
 
-namespace beast = boost::beast;    // from <boost/beast.hpp>
-namespace http = beast::http;      // from <boost/beast/http.hpp>
-namespace net = boost::asio;       // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;  // from <boost/asio/ip/tcp.hpp>
-using nlohmann::json;
+using json = nlohmann::json;
 
-#ifndef INCLUDE_SUGGEST_HPP_
-#define INCLUDE_SUGGEST_HPP_
+class Suggestions_collection {
+ public:
+  Suggestions_collection();
+  void Update(json storage);
+  json Suggest(const std::string& input);
+
+ private:
+  json suggestions_;
+};
+#endif // INCLUDE_HEADER_HPP_
 
 class Server {
  public:
