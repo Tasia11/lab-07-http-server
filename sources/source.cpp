646ccbea317b12db13fe448971f78a584c3d1a1b
Tasia11 <61164926+Tasia11@users.noreply.github.com>
2021-01-21 08:14:27 +0300

    Update source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,83 +1,42 @@
 // Copyright 2020 Your Name <your_email>
 
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/config.hpp>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
 #include "header.hpp"
+#include "json_storage.hpp"
 
-boost::shared_mutex m;
-json suggestions;
+namespace beast = boost::beast;    // from <boost/beast.hpp>
+namespace http = beast::http;      // from <boost/beast/http.hpp>
+namespace net = boost::asio;       // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;  // from <boost/asio/ip/tcp.hpp>
 
-bool sortJson(json& a, json& b) {
-  if (a.at("cost").get<int>() > b.at("cost").get<int>()) return true;
-  return false;
+std::string make_json(const json& data) {
+  std::stringstream ss;
+  if (data.is_null())
+    ss << "No suggestions";
+  else
+    ss << std::setw(4) << data;
+  return ss.str();
 }
-
-void write() {
-  std::ifstream file("./v1/api/suggest/suggestions.json");
-  boost::lock_guard<boost::shared_mutex> lk(m);
-  // suggestions.clear();
-  json temp;
-  try {
-    file >> temp;
-    suggestions = temp;
-    std::cout << "Collections is updated" << std::endl;
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
-  file.close();
-}
-
-void update(clock_t startTime) {
-  for (;;) {
-    clock_t presentTime = clock();
-    if ((static_cast<double>(presentTime - startTime)) / CLOCKS_PER_SEC == 5) {
-      startTime = presentTime;
-      write();
-    }
-  }
-}
-
-json read(std::string&& word) {
-  json formData = json::parse("{ \"suggestions\": []}");
-  m.try_lock_shared();
-  json temp;
-  // lk.lock();
-  for (const auto& i : suggestions) {
-    if (i.at("name").get<std::string>().find(word) != std::string::npos)
-      temp.push_back(i);
-  }
-  m.unlock_shared();
-  std::sort(temp.begin(), temp.end(), sortJson);
-  size_t num = 0;
-  for (const auto& elem : temp) {
-    formData.at("suggestions")
-        .push_back(json::parse(R"({ "text": ")" +
-                               elem.at("name").get<std::string>() + "\"," +
-                               "\"position\": " + std::to_string(num) + "}"));
-    ++num;
-  }
-  return formData;
-}
-
-std::string path_cat(beast::string_view base, beast::string_view path) {
-  if (base.empty()) return std::string(path);
-  std::string result(base);
-#ifdef BOOST_MSVC
-  char constexpr path_separator = '\\';
-  if (result.back() == path_separator) result.resize(result.size() - 1);
-  result.append(path.data(), path.size());
-  for (auto& c : result)
-    if (c == '/') c = path_separator;
-#else
-  char constexpr path_separator = '/';
-  if (result.back() == path_separator) result.resize(result.size() - 1);
-  result.append(path.data(), path.size());
-#endif
-  return result;
-}
-
+// This function produces an HTTP response for the given
+// request. The type of the response object depends on the
+// contents of the request, so the interface requires the
+// caller to pass a generic lambda for receiving the response.
 template <class Body, class Allocator, class Send>
-void handle_request(beast::string_view doc_root,
-                    http::request<Body, http::basic_fields<Allocator>>&& req,
-                    Send&& send) {
+void handle_request(http::request<Body, http::basic_fields<Allocator>>&& req,
+                    Send&& send, const std::shared_ptr<std::timed_mutex>& mutex,
+                    const std::shared_ptr<Suggestions_collection>& collection) {
+  // Returns a bad request response
   auto const bad_request = [&req](beast::string_view why) {
     http::response<http::string_body> res{http::status::bad_request,
                                           req.version()};
@@ -89,6 +48,7 @@ void handle_request(beast::string_view doc_root,
     return res;
   };
 
+  // Returns a not found response
   auto const not_found = [&req](beast::string_view target) {
     http::response<http::string_body> res{http::status::not_found,
                                           req.version()};
@@ -100,57 +60,65 @@ void handle_request(beast::string_view doc_root,
     return res;
   };
 
-  auto const server_error = [&req](beast::string_view what) {
-    http::response<http::string_body> res{http::status::internal_server_error,
-                                          req.version()};
-    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-    res.set(http::field::content_type, "text/html");
-    res.keep_alive(req.keep_alive());
-    res.body() = "An error occurred: '" + std::string(what) + "'";
-    res.prepare_payload();
-    return res;
-  };
+  if (req.method() == http::verb::get) {
+    return send(bad_request("Hello!"));
+  }
 
-  if (req.method() != http::verb::post)
-    return send(bad_request("Unknown HTTP-method for this app"));
+  if (req.method() != http::verb::post && req.method() != http::verb::head) {
+    return send(bad_request("Unknown HTTP-method"));
+  }
 
-  if (req.target().empty() || req.target()[0] != '/' ||
-      req.target().find("..") != beast::string_view::npos)
-    return send(bad_request("Illegal request-target"));
-
-  // Build the path to the requested file
-  std::string path = path_cat(doc_root, req.target());
-  nlohmann::json j = nlohmann::json::parse(req.body());
-  json bodyOut = read(j.at("input").get<std::string>());
-  if ((req.target() == "/v1/api/suggest/") ||
-      (req.target() == "/v1/api/suggest"))
-    path.append("suggestions.json");
-
-  // Attempt to open the file
-  beast::error_code ec;
-  http::file_body::value_type body;
-  body.open(path.c_str(), beast::file_mode::scan, ec);
-  // Handle the case where the file doesn't exist
-  if (ec == beast::errc::no_such_file_or_directory) {
+  if (req.target() != "/v1/api/suggest") {
     return send(not_found(req.target()));
   }
 
-  // Handle an unknown error
-  if (ec) return send(server_error(ec.message()));
+  json input_body;
+  try {
+    input_body = json::parse(req.body());
+  } catch (std::exception& e) {
+    return send(bad_request(e.what()));
+  }
+  boost::optional<std::string> input;
+  try {
+    input = input_body.at("input").get<std::string>();
+  } catch (std::exception& e) {
+    return send(bad_request(R"(JSON format: {"input": "<user_input>"}!)"));
+  }
+  if (!input.has_value()) {
+    return send(bad_request(R"(JSON format: {"input": "<user_input>"}!)"));
+  }
 
-  // Cache the size since we need it after the move
+  mutex->lock();
+  auto result = collection->Suggest(input.value());
+  mutex->unlock();
+  http::string_body::value_type body = make_json(result);
   auto const size = body.size();
-  // Respond to POST request
-  http::response<http::string_body> res{http::status::ok, req.version()};
+
+  if (req.method() == http::verb::head) {
+    http::response<http::empty_body> res{http::status::ok, req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "application/json");
+    res.content_length(size);
+    res.keep_alive(req.keep_alive());
+    return send(std::move(res));
+  }
+
+  http::response<http::string_body> res{
+      std::piecewise_construct, std::make_tuple(std::move(body)),
+      std::make_tuple(http::status::ok, req.version())};
   res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
   res.set(http::field::content_type, "application/json");
   res.content_length(size);
   res.keep_alive(req.keep_alive());
-  res.body() = bodyOut.dump(1, ' ');
-  res.prepare_payload();
   return send(std::move(res));
 }
 
+void fail(beast::error_code ec, char const* what) {
+  std::cerr << what << ": " << ec.message() << "\n";
+}
+
+// This is the C++11 equivalent of a generic lambda.
+// The function object is used to send an HTTP message.
 template <class Stream>
 struct send_lambda {
   Stream& stream_;
@@ -173,70 +141,92 @@ struct send_lambda {
   }
 };
 
-void fail(beast::error_code ec, char const* what) {
-  std::cerr << what << ": " << ec.message() << "\n";
-}
-
-void do_session(tcp::socket& socket,
-                std::shared_ptr<std::string const> const& doc_root) {
+void do_session(net::ip::tcp::socket& socket,
+                const std::shared_ptr<Suggestions_collection>& collection,
+                const std::shared_ptr<std::timed_mutex>& mutex) {
   bool close = false;
   beast::error_code ec;
 
+  // This buffer is required to persist across reads
   beast::flat_buffer buffer;
 
+  // This lambda is used to send messages
   send_lambda<tcp::socket> lambda{socket, close, ec};
-
   for (;;) {
+    // Read a request
     http::request<http::string_body> req;
     http::read(socket, buffer, req, ec);
     if (ec == http::error::end_of_stream) break;
     if (ec) return fail(ec, "read");
 
-    handle_request(*doc_root, std::move(req), lambda);
+    // Send the response
+    handle_request(std::move(req), lambda, mutex, collection);
     if (ec) return fail(ec, "write");
-    if (close) {
-      break;
-    }
   }
+
+  // Send a TCP shutdown
   socket.shutdown(tcp::socket::shutdown_send, ec);
 }
 
-int Server::startServer(int argc, char* argv[]) {
-  try {
-    if (argc != 4) {
-      std::cerr
-        << "\nUsage: http-server-sync <address> <port> <doc_root>\n"
-        << "Example:\n"
-        << "    http-server-sync 0.0.0.0 8080 .\n";
-      return EXIT_FAILURE;
-    }
-
-    auto const address = net::ip::make_address(argv[1]);
-    port = static_cast<uint16_t>(std::atoi(argv[2]));
-    doc_root = std::make_shared<std::string>(argv[3]);
-    net::io_context ioc{1};
-
-    tcp::acceptor acceptor{ioc, {address, port}};
-
-    clock_t beginTime = clock();
-
-    std::thread{std::bind(&update, beginTime)}.detach();
-
-    for (;;) {
-      tcp::socket socket{ioc};
-      acceptor.accept(socket);
-
-      std::thread{std::bind(&do_session, std::move(socket),
-                            doc_root)}.detach();
-    }
-  } catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
+void suggestion_updater(
+    const std::shared_ptr<json_storage>& storage,
+    const std::shared_ptr<Suggestions_collection>& suggestions,
+    const std::shared_ptr<std::timed_mutex>& mutex) {
+ // using std::chrono_literals::operator""min;
+  using namespace std::literals::chrono_literals;
+  for (;;) {
+    // WRITE - блокировка
+    mutex->lock();
+    storage->Load();
+    suggestions->Update(storage->get_storage());
+    mutex->unlock();
+    std::cout << "Suggestions updated" << std::endl;
+    std::this_thread::sleep_for(15min);
   }
-  return 0;
 }
 
-Server::Server() {
-  std::ifstream file("./v1/api/suggest/suggestions.json");
-  file >> suggestions;
-  file.close();
+int Run_server(int argc, char* argv[]) {
+  std::shared_ptr<std::timed_mutex> mutex =
+      std::make_shared<std::timed_mutex>();
+  std::shared_ptr<json_storage> storage = std::make_shared<json_storage>(
+      "/home/asya-tanis/CLionProjects/lab-07-http-server/suggestions.json");
+  std::shared_ptr<Suggestions_collection> suggestions =
+      std::make_shared<Suggestions_collection>();
+  try {
+    // Check command line arguments.
+    if (argc != 3) {
+      std::cerr << "Usage: suggestion_server <address> <port>\n"
+                << "Example:\n"
+                << "    http-server-sync 0.0.0.0 8080\n";
+      return EXIT_FAILURE;
+    }
+    auto const address = net::ip::make_address(argv[1]);
+    auto const port = static_cast<uint16_t>(std::atoi(argv[2]));
+
+    // The io_context is required for all I/O
+    net::io_context ioc{1};
+
+    // The acceptor receives incoming connections
+    tcp::acceptor acceptor{ioc, {address, port}};
+
+    std::thread{suggestion_updater, storage, suggestions, mutex}.detach();
+    for (;;) {
+      // This will receive the new connection
+      tcp::socket socket{ioc};
+
+      // Block until we get a connection
+      acceptor.accept(socket);
+
+      // Launch the session, transferring ownership of the socket
+      std::thread{std::bind(&do_session, std::move(socket), suggestions, mutex)}
+          .detach();
+    }
+  } catch (std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+}
+// ./cmake-build-debug/tests 0.0.0.0 8080
+int main(int argc, char* argv[]) {
+  return Run_server(argc, argv);
 }
